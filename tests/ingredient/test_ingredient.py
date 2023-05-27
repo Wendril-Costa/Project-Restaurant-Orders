ca6f658d7db693ce093e2a728955bcdcb03b949b
Wendril-Costa <wendrild.c.n@gmail.com>
2023-05-27 04:01:22 -0300

    Requisito 1 feito

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,20 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient = Ingredient("bacon")
+    assert ingredient.name == "bacon"
+    assert ingredient.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert repr(ingredient) == "Ingredient('bacon')"
+    assert ingredient == Ingredient("bacon")
+    assert ingredient != Ingredient("cheese")
+    assert hash(ingredient) == hash(Ingredient("bacon"))
+    assert hash(ingredient) != hash(Ingredient("cheese"))
+    assert isinstance(ingredient, Ingredient)
